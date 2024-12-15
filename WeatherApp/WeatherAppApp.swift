2f598328c8fcadc5cb50805a7761a316d484a85a
Nikita Plakhin <nikita.plakhin@effective-soft.com>
2024-12-15 16:45:11 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Никита Плахин on 15.12.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
